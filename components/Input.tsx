da4c9141337250a134ff629b35526bcbafa531f9
Jack Pickard <jack@hertilityhealth.com>
2022-10-28 13:42:31 +0100

    error

--- components/Input.tsx
+++ components/Input.tsx
@@ -8,16 +8,29 @@ interface Props {
 
 export const Input: React.FC<Props> = ({ onClick }) => {
   const [text, setText] = React.useState("");
+  const [error, setError] = React.useState(false);
   return (
     <div className="inputContainer">
       <input
         className="input"
         type="text"
-        onChange={(e) => setText(e.target.value)}
+        onChange={(e) => {
+          const spaceCheck = /\s/g;
+          const regTest = spaceCheck.test(e.target.value);
+          if (regTest) {
+            console.log("true");
+            setError(true);
+          } else {
+            console.log("nope");
+            setError(false);
+            setText(e.target.value);
+          }
+        }}
       ></input>
       <button className="button" onClick={() => onClick(text)}>
         Click to view results
       </button>
+      {error && <div className="error">No spaces in the postcode please</div>}
     </div>
   );
 };
